2e05e66a75609951c63363809901634fe5d0154d
AmalijaN <amalija.novalic@kodelab.hr>
2023-02-26 19:58:22 +0100

    first commit

---
+++ lib/matrix_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MatrixContorller extends GetxController {
+  List<int> matrix = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].obs;
+  matrixUpdate(m){
+
+  }
+}
